6e5ea89570f7b13138d9edf114f8aef2bc881a2d
LBjerke <loyde.b.bjerke@gmail.com>
2017-06-01 20:36:08 -0600

    added more unit tests

--- src/com/hollowknight/HollowCore/BasicOperators.java
+++ src/com/hollowknight/HollowCore/BasicOperators.java
@@ -6,16 +6,32 @@ package com.hollowknight.HollowCore;
 
 public class BasicOperators {
 
+    //adds b to a
     public double basicAdd(double a , double b) {
     double c;
     c = a + b;
     return c;
     }
+    //subtracks b from a
     public double basicSubtract( double a , double b)
     {
         double c;
         c = a - b;
         return c;
     }
+    //multiplies a by b
+     public double basicMultiply(double a , double b)
+     {
+         double c;
+         c = a * b;
+         return c;
+     }
+     //divides a by b
+     public double basicDivide(double a , double b)
+     {
+         double c;
+         c = (b==0)? 0 : a/b;
+         return c;
+     }
 
 }
--- src/com/hollowknight/HollowCore/BasicOperatorsTest.java
+++ src/com/hollowknight/HollowCore/BasicOperatorsTest.java
@@ -1,5 +1,6 @@
 package com.hollowknight.HollowCore;
 
+import org.bouncycastle.jcajce.provider.asymmetric.ec.KeyFactorySpi;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,6 +17,21 @@ public class BasicOperatorsTest {
     public void basicSubtract() throws Exception{
         BasicOperators op = new BasicOperators();
         Assert.assertEquals(10 , op.basicSubtract(30,20), 0.1);
+    }
+    @Test
+    public void basicMultiply() throws Exception{
+        BasicOperators op = new BasicOperators();
+        Assert.assertEquals(10, op.basicMultiply(10,1),0.1);
+    }
+    @Test
+    public void basicDivide() throws Exception{
+        BasicOperators op = new BasicOperators();
+        Assert.assertEquals(5 , op.basicDivide(25,5),0.1);
+    }
+    @Test
+    public void basicDivideByZero() throws Exception{
+        BasicOperators op = new BasicOperators();
+        Assert.assertEquals(0 , op.basicDivide(10, 0), 0.0001);
     }
 
 }
